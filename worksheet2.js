74a52c1d939e7b46892d8bb4a75892a988071940
Sandy-NEP <sg9816062923@gmail.com>
2025-06-13 13:51:45 +0545

    sandesh

--- worksheet2.js
+++ worksheet2.js
@@ -1,4 +1,7 @@
 // Worksheet 2: Operators and Expressions
+
+
+//1.arithmetic operators
 let a = 50;
 let b = 8;
 let c = 2;
@@ -21,6 +24,7 @@ console.log("Remainder:", remainder);
 console.log("Exponent:", exponent);
 console.log("Incremented a:", increment);
 console.log("Decremented b:", decrement);
+console.log("\n\n\n");
 
 /*output:
 Sum: 60
@@ -33,3 +37,76 @@ Incremented a: 51
 Decremented b: 7
 */
 // This code demonstrates the use of arithmetic operators in JavaScript.
+
+// 2.Assignment Operators
+
+let x = 8;
+let y = 2;
+
+x += y; // 10
+console.log("x += y :", x);
+
+y = 5;
+y -= x; // -5
+console.log("y -= x :", y);
+
+x = 4;
+x *= y; // -20
+console.log("x *= y :", x);
+
+y = 10;
+y /= x; // -0.5
+console.log("y /= x :", y);
+
+x = 9;
+y = 4;
+x %= y; // 1
+console.log("x %= y :", x);
+
+x = 2;
+y = 3;
+y **= x; // 9
+console.log("y **= x :", y);
+
+console.log("\n\n\n");
+//output:
+// x += y : 10
+// y -= x : -5
+// x *= y : -20
+// y /= x : -0.5
+// x %= y : 1
+// y **= x : 9
+
+
+
+// 3.Relational Operators
+
+let num1 = 10;
+let num2 = 20;
+
+let name1 = "sandesh";
+let name2 = "sandesh";
+let name3 = "ghimire";
+
+console.log("num1 < num2:", num1 < num2); // less than true
+console.log("num1 <= num2:", num1 <= num2); // less than or equal to true
+console.log("num1 > num2:", num1 > num2); // greater than false
+console.log("num1 >= num2:", num1 >= num2); // greater than or equal to false
+
+console.log("name1 == name2:", name1 == name2); // true
+
+console.log("name1 === name2:", name1 === name2); // true
+console.log("name1 != name3:", name1 != name3); // true
+console.log("name1 !== name3:", name1 !== name2); // false
+
+// output:
+// num1 < num2: true
+// num1 <= num2: true
+// num1 > num2: false
+// num1 >= num2: false
+// name1 == name2: true
+// name1 === name2: true
+// name1 != name3: true
+// name1 !== name3: false
+
+// 4.Logical Operators
