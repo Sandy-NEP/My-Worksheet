aca86a18bd35eecc53379c9fb0027102ed13bb8f
Sandy-NEP <sg9816062923@gmail.com>
2025-06-13 12:56:07 +0545

    sandesh

---
+++ worksheet2.js
@@ -0,0 +1,35 @@
+// Worksheet 2: Operators and Expressions
+let a = 50;
+let b = 8;
+let c = 2;
+
+let sum = a + b + c; // Adding a, b, and c
+let difference = a - b - c; // Subtracting b and c from a
+let product = a * b * c; // Multiplying a, b, and c
+let quotient = a / b / c; // Dividing a by b and then by c
+let remainder = a % b; // Finding the remainder of a divided by b
+let exponent = a ** c; // Raising a to the power of c
+let increment = ++a; // Incrementing a by 1
+let decrement = --b; // Decrementing b by 1
+
+
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
+console.log("Remainder:", remainder);
+console.log("Exponent:", exponent);
+console.log("Incremented a:", increment);
+console.log("Decremented b:", decrement);
+
+/*output:
+Sum: 60
+Difference: 40
+Product: 800
+Quotient: 3.125
+Remainder: 2
+Exponent: 125000
+Incremented a: 51
+Decremented b: 7
+*/
+// This code demonstrates the use of arithmetic operators in JavaScript.
