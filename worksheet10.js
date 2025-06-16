3cb6fa13d8e2e1f7a1919ef7ff18dc56d4ec6a2b
Sandy-NEP <sg9816062923@gmail.com>
2025-06-16 14:38:29 +0545

    Last submit

--- worksheet10.js
+++ worksheet10.js
@@ -5,6 +5,7 @@ let doubled = numbers.map(n => n * 2);
 let filtered = numbers.filter(n => n > 3);
 let found = numbers.find(n => n > 3);
 let sum = numbers.reduce((acc, n) => acc + n, 0);
+
 console.log("Original:", numbers, "\nDoubled:", doubled, "\nFiltered:", filtered, "\nFound:", found, "\nSum:", sum);
 
 /*
@@ -14,6 +15,7 @@ filter()	Picked only items > 3
 find()	Found the first item > 3
 reduce()	Added them all into one value
 */
+
 // Output:
 // Original: [1, 2, 3, 4, 5]
 // Doubled: [2, 4, 6, 8, 10]
